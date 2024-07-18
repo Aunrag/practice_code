587973dc5a28ac62b8a937c8388ad420c54f2548
Anurag <pawaranurag9145@gmail.com>
2024-07-18 16:54:09 +0530

    conatiner with most water

---
+++ fishinriver/containerwithmostwater.py
@@ -0,0 +1,22 @@
+class maxarea():
+    def area(self):
+        n=int(input("enter number of lines: "))
+        import array as arr 
+        a=arr.array('i',[])
+        for i in range(0,n):
+            c=int(input("enter the height: "))
+            a.append(c)
+        A=0  
+        for i in range(0,n):
+            s=a[i]
+            for j in range(i+1,n):
+                d=a[j]
+                area=((j-i)*(min(s,d)))
+                A=max(A,area)
+                
+        print(A) 
+    
+        
+height=maxarea()
+height.area()
+      
